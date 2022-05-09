a2790921610bfa6c43b831c0a6089ccda22df5a7
Matdrox <Matdrox@users.noreply.github.com>
2022-05-09 12:25:45 +0200

    Updated Header Files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <iostream>                     // <> --> include C++ libraries
+#include "includes/calculations.h"    // "" --> include own libraries
 
 int main() {
-    std::cout << "Hello World";
+    std::cout << add(10, 20) << std::endl;
     return 0;
 }
