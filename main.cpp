2ab09ccc0efc3c9784c342393b477134dfb98940
Matdrox <Matdrox@users.noreply.github.com>
2022-05-13 13:49:39 +0200

    Finished CPP4PY 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*
+Interpreter - Fast start, slow run, line by line
+Compiler - Slow start, fast run, all the file at once
+*/
+
 #include <iostream>
 // #include <libraryname>   // Libraries or headers provided by implementation
 // #include "filename"      // Libraries or headers that are cutsom made
@@ -7,9 +12,9 @@ int main() {  // The OS automatically calls the main() function. Neccessary!
     float num;
     cout << "Give me a number: ";  // Output Operator <<
 
-    cin >> num;
+    cin >> num;  // Input operator >>
 
-    cout << "This is your number doubled: " << num*2 << endl;
+    cout << "This is your number doubled: " << num * 2 << endl;  // << acts like python's +
 
     return 0;
 }
